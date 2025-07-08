08c9b4ef20dcf560f8db31394d3d0bc41d09c460
pkaquan <mam4ch333@gmail.com>
2025-07-09 01:27:57 +0700

    Update Control_Device.cpp

--- src/Device/Control_Device.cpp
+++ src/Device/Control_Device.cpp
@@ -17,11 +17,9 @@ void ControlDevice ::switchDevice (byte value) {
     digitalWrite (pinLatch, HIGH);
 }
 void ControlDevice :: buzzerDevice (int value) {
-    float voltage = value * 3.3 / 4095.0;
-    Serial.print (voltage);
-    if (voltage == 0.00){
-        digitalWrite(pinBuzzer, HIGH);
-    } else  if (voltage > 0){
+    if (value >=){
+        digitalWrite(pinBuzzer,HIGH);
+    } else{
         digitalWrite(pinBuzzer,LOW);
     }
 }
@@ -30,14 +28,6 @@ void ControlDevice :: pwmDevice (bool state, int hour, int minute, int time, int
     client.update();
     currentHour = client.getHours();
     currentMinute = client.getMinutes();
-    Serial.println (currentHour);
-    Serial.println (currentMinute);
-    Serial.println ("Trang thai bom");
-    Serial.println (state);
-    Serial.println (hour);
-    Serial.println (minute);
-    Serial.println (time);
-    Serial.println (speed);
     if (state == true && hour == currentHour  && minute == currentMinute){
         digitalWrite(pinPWM, HIGH);
         vTaskDelay(pdMS_TO_TICKS(time * 60 *1000));  
