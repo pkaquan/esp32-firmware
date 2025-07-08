bb5583fef34688e8da8795bf6d0d3d36b23374c3
pkaquan <mam4ch333@gmail.com>
2025-07-09 05:49:23 +0700

    Update Firebase.cpp

--- src/Service/Firebase.cpp
+++ src/Service/Firebase.cpp
@@ -2,6 +2,8 @@
 
 #include "Firebase.h"
 #include "addons/TokenHelper.h"
+
+//Obtain user credentials to connect to Firebase
 void Service::begin (String setEmail, String setPassword){
     email = setEmail;
     password = setPassword;
@@ -19,6 +21,7 @@ void Service::begin (String setEmail, String setPassword){
     documentPath += auth.token.uid.c_str();
  };
 
+//Send an integer value to the cloud
 void Service:: setIntValue (String setJsonFirestore, int intValue){
     String path = "fields/";
     path += setJsonFirestore;
@@ -26,6 +29,8 @@ void Service:: setIntValue (String setJsonFirestore, int intValue){
     json.set(path.c_str(),intValue);
     Firebase.Firestore.patchDocument(&fbdo,projectID,"",documentPath.c_str(),json.raw(),setJsonFirestore.c_str());
 }
+
+//Send a boolean value to the cloud
 void Service:: setBoolValue (String setJsonFirestore, bool boolValue){
     String path = "fields/";
     path += setJsonFirestore;
@@ -33,6 +38,8 @@ void Service:: setBoolValue (String setJsonFirestore, bool boolValue){
     json.set(path.c_str(),boolValue);
     Firebase.Firestore.patchDocument(&fbdo,projectID,"",documentPath.c_str(),json.raw(),setJsonFirestore.c_str());  
 }
+
+//Retrieve an integer value from the cloud
 bool Service ::getBool (String getJsonFirestore){
     String path = "fields/";
     path += getJsonFirestore;
@@ -48,6 +55,8 @@ bool Service ::getBool (String getJsonFirestore){
     }
     return false;
 }
+
+//Retrieve a boolean value from the cloud
 int Service :: getInt (String getJsonFirestore){
     String path = "fields/";
     path += getJsonFirestore;
